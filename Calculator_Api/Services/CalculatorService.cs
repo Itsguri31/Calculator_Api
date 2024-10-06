b239f6515cccb763328d01ae1ed126daeecd315a
Itsguri31 <gurkirats0001@gmail.com>
2024-10-06 20:54:23 +0000

    methods added in service class

--- Calculator_Api/Services/CalculatorService.cs
+++ Calculator_Api/Services/CalculatorService.cs
@@ -2,4 +2,21 @@ namespace CalculatorApi.Calculator;
 
 public class CalculatorService : ICalculatorService <decimal>{
 
+    public int Add(decimal left, decimal right) {
+        return left + right;
+    }
+
+    public int Subtract(decimal left, decimal right) {
+        return left - right;
+    }
+
+    public int Divide(decimal left, decimal right) {
+        return left / right;
+    }
+
+
+    public int Multiply(decimal left, decimal right) {
+        return left * right;
+    }
+
 }
